ba2b3293852a8bdba982417f93523d4af21f10ca
larrayoz146585 <148328956+larrayoz146585@users.noreply.github.com>
2025-12-01 19:04:08 +0100

    Añadido ssh

--- machine/src/execute.sh
+++ machine/src/execute.sh
@@ -9,5 +9,11 @@ service apache2 start
 echo "[+] Iniciando Cron..."
 service cron start
 
+# --- BLOQUE NUEVO PARA SSH ---
+echo "[+] Iniciando SSH..."
+mkdir -p /run/sshd   
+service ssh start
+# -----------------------------
+
 echo "[+] Sistema Napping listo."
 tail -f /var/log/apache2/access.log
