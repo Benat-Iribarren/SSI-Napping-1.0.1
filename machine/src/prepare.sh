8ecc3f37d0c5bf35fb57347279a3a51fbecb78a5
larrayoz146585 <148328956+larrayoz146585@users.noreply.github.com>
2025-12-01 18:56:51 +0100

    Añadido ssh

--- machine/src/prepare.sh
+++ machine/src/prepare.sh
@@ -9,17 +9,16 @@ apt-get update && apt-get install -y \
     libapache2-mod-php \
     python3 \
     python3-requests \
+    openssh-server \
     cron \
     nano \
     sudo \
     && apt-get clean
 
 # 2. CREACIÓN DE USUARIOS
-# ADRIAN (UID 1000)
 useradd -u 1000 -m -s /bin/bash adrian
 echo "adrian:adrian" | chpasswd
 
-# DANIEL (UID 1001)
 useradd -u 1001 -m -s /bin/bash daniel
 echo "daniel:daniel" | chpasswd
 
@@ -33,18 +32,16 @@ chown -R root:root /var/www/html
 chmod -R 755 /var/www/html
 chmod 644 /var/www/html/config.php
 
-# 5. COLOCACIÓN DEL SCRIPT VULNERABLE
+# 5. SCRIPT VULNERABLE
 if [ -f /opt/src/query.py ]; then
     mv /opt/src/query.py /home/adrian/query.py
     chown adrian:adrian /home/adrian/query.py
     chmod 777 /home/adrian/query.py
-else
-    echo "AVISO: No se encontró query.py"
 fi
 
-# 6. CONFIGURACIÓN DEL CRON (LA VULNERABILIDAD)
+# 6. CRON
 echo "*/2 * * * * adrian /usr/bin/python3 /home/adrian/query.py" > /etc/cron.d/vuln_task
 chmod 644 /etc/cron.d/vuln_task
 
-# 7. CARPETAS DE FLAGS
+# 7. FLAGS
 mkdir -p /flag
