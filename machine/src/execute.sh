cf34d5d136808bce98fbea8b5b839a87331b02a7
larrayoz146585 <148328956+larrayoz146585@users.noreply.github.com>
2025-12-12 10:48:23 +0100

    Subida inicial de versión funcional desde Linux

---
+++ machine/src/execute.sh
@@ -0,0 +1,41 @@
+#! /bin/bash
+
+echo "[+] Iniciando MariaDB..."
+service mariadb start
+
+echo "[+] Iniciando Apache..."
+service apache2 start
+
+echo "[+] Iniciando Cron..."
+service cron start
+
+echo "[+] Iniciando SSH..."
+mkdir -p /run/sshd
+service ssh start
+
+echo "[+] Colocando flags en sus directorios..."
+
+# 1. MOVER FLAG DE ROOT A /root/
+if [ -f /flag/root.txt ]; then
+    cp /flag/root.txt /root/root.txt
+    chown root:root /root/root.txt
+    chmod 600 /root/root.txt
+    rm /flag/root.txt
+fi
+
+# 2. MOVER FLAG DE USUARIO A /home/daniel/
+if [ -f /flag/user.txt ]; then
+    cp /flag/user.txt /home/daniel/user.txt
+    chown daniel:daniel /home/daniel/user.txt
+    chmod 600 /home/daniel/user.txt
+    rm /flag/user.txt
+fi
+# . MOVER FLAG DE USUARIO A /home/adrian/
+if [ -f /flag/flag.txt ]; then
+    cp /flag/flag.txt /home/adrian/flag.txt
+    chown adrian:adrian /home/adrian/flag.txt
+    chmod 600 /home/adrian/flag.txt
+    rm /flag/flag.txt
+fi
+echo "[+] Sistema Napping listo."
+tail -f /var/log/apache2/access.log
---
+++ machine/src/prepare.sh
@@ -0,0 +1,57 @@
+#! /bin/bash
+
+# 1. INSTALACIÓN DE PAQUETES
+apt-get update && apt-get install -y \
+    apache2 \
+    mariadb-server \
+    php \
+    php-mysql \
+    libapache2-mod-php \
+    python3 \
+    python3-requests \
+    openssh-server \
+    cron \
+    nano \
+    sudo \
+    vim \
+    && apt-get clean
+
+# 2. CREACIÓN DE USUARIOS
+useradd -u 1000 -m -s /bin/bash adrian
+echo "adrian:adrian" | chpasswd
+
+groupadd -g 1002 administrators
+useradd -u 1001 -m -s /bin/bash -G administrators daniel
+echo "daniel:C@ughtm3napping123" | chpasswd
+
+# 3. BASE DE DATOS
+service mariadb start
+mysql -u root < /opt/src/schema.sql
+
+# 4. CONFIGURACIÓN WEB
+rm -f /var/www/html/index.html
+chown -R root:root /var/www/html
+chmod -R 755 /var/www/html
+chmod 644 /var/www/html/config.php
+
+# 5. SCRIPT VULNERABLE
+if [ -f /opt/src/query.py ]; then
+    mv /opt/src/query.py /home/adrian/query.py
+
+    chown adrian:administrators /home/adrian/query.py
+    
+    #permisos 775: El dueño (adrian) y el grupo (administrators/daniel) pueden escribir
+    chmod 775 /home/adrian/query.py
+else
+    echo "AVISO: No se encontró query.py"
+fi
+
+# 6. CRON
+echo "*/2 * * * * adrian /usr/bin/python3 /home/adrian/query.py" > /etc/cron.d/vuln_task
+chmod 644 /etc/cron.d/vuln_task
+
+#7 meto a adrian en sudoers para que pueda editar archivos con vim
+echo "adrian ALL=(root) NOPASSWD: /usr/bin/vim" >> /etc/sudoers
+
+# 7. FLAGS
+mkdir -p /flag
